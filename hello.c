e0cf9932f7f0f3fe9837ef5412708167eade92b9
Abhiram <abhiramreddi666@gmail.com>
2024-09-22 12:53:28 +0530

    learning how to push into remote repos

--- hello.c
+++ hello.c
@@ -1,4 +1,4 @@
 #inlcude<stdio.h>
 int main(){
-  printf("I am updating in remote and trying to merge it with local through termoinal");
+  printf("I am learning how to push from local to remote");
 }
